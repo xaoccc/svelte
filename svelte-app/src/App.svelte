e8a6626f2e9602828c2be0f9380c8240b598745b
xaoccc <xaocccc@gmail.com>
2025-08-27 10:26:45 +0300

    group bindings

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -3,6 +3,7 @@
 
     import Lesson_18 from './lib/Lesson_18/Lesson_18.svelte';
     import Lesson_19 from './lib/Lesson_19.svelte';
+    import Lesson_21 from './lib/Lesson_21.svelte';
 
 </script>
 
@@ -16,7 +17,7 @@
   <h3>Each component will be a new lesson</h3>
 
   <div class="card">
-    <Lesson_19 />
+    <Lesson_21 />
   </div>
 
 </main>
