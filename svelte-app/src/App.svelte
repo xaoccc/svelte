a25615bd0283800f4eca2451f168d4e744f0c487
xaoccc <xaocccc@gmail.com>
2025-08-27 14:53:52 +0300

    svelte classes

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -4,6 +4,7 @@
     import Lesson_18 from './lib/Lesson_18/Lesson_18.svelte';
     import Lesson_19 from './lib/Lesson_19.svelte';
     import Lesson_21 from './lib/Lesson_21.svelte';
+    import Lesson_22 from './lib/Lesson_22/Lesson_22.svelte';
 
 </script>
 
@@ -17,7 +18,7 @@
   <h3>Each component will be a new lesson</h3>
 
   <div class="card">
-    <Lesson_21 />
+    <Lesson_22 />
   </div>
 
 </main>
