104bbe5a3a954f0816ebecc74f94c50f76fd9635
xaoccc <xaocccc@gmail.com>
2025-09-01 10:03:25 +0300

    transition

---
+++ svelte-app/src/lib/Lesson_27.svelte
@@ -0,0 +1,17 @@
+<!-- The transition directive -->
+
+<script>
+	import { fade } from 'svelte/transition'
+	let visible = $state(true);
+</script>
+
+<label>
+	<input type="checkbox" bind:checked={visible} />
+	visible
+</label>
+
+{#if visible}
+	<p transition:fade>
+		Fades in and out
+	</p>
+{/if}
